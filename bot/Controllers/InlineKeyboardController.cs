09fe86a300fd0a6e3edab1ff5d00da4201b3fa8e
Name <email@exaple.ru>
2023-08-08 08:37:39 +0300

    InlineKeyboardController

---
+++ bot/Controllers/InlineKeyboardController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+
+namespace bot.Controllers
+{
+    public class InlineKeyboardController
+    {
+        private readonly IStorage _memoryStorage;
+        private readonly ITelegramBotClient _telegramClient;
+
+        public InlineKeyboardController(ITelegramBotClient telegramBotClient,
+            IStorage memoryStorage)
+        {
+            _telegramClient = telegramBotClient;
+            _memoryStorage = memoryStorage;
+        }
+        public async Task Handle(CallbackQuery? callbackQuery, CancellationToken ct)
+        {
+            if (callbackQuery?.Data == null) return;
+
+            _memoryStorage.GetSession(callbackQuery.From.Id).LanguageCode = callbackQuery.Data;
+
+            string languageText = callbackQuery.Data switch
+            {
+                "ru" => "Русский",
+                "en" => "Английский",
+                _ => String.Empty
+            };
+
+            await
+                _telegramClient.SendTextMessageAsync(callbackQuery.From.Id,
+                $"<b>Язык аудио - {languageText}.{Environment.NewLine}</b>" +
+                $"{Environment.NewLine} Можно поменять в главном меню.", cancellationToken: ct,
+                parseMode: ParseMode.Html);
+        }
+    }
+}
