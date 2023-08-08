4c4ecb59dd25496b266c862499bf6714eeb28ff9
Name <email@exaple.ru>
2023-08-08 09:24:27 +0300

    Подсчет символов

--- bot/Controllers/TextMessageController.cs
+++ bot/Controllers/TextMessageController.cs
@@ -7,15 +7,20 @@ using Telegram.Bot.Types;
 using Telegram.Bot;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
+using bot.Models;
+using bot.Controllers;
+using System.Threading;
 
 namespace bot.Controllers
 {
     public class TextMessageController
     {
+        private readonly IStorage _memoryStorage;
         private readonly ITelegramBotClient _telegramClient;
 
-        public TextMessageController(ITelegramBotClient telegramBotClient)
+        public TextMessageController(ITelegramBotClient telegramBotClient, IStorage memoryStorage)
         {
+            _memoryStorage = memoryStorage;
             _telegramClient = telegramBotClient;
         }
         public async Task Handle(Message message, CancellationToken ct)
@@ -36,9 +41,13 @@ namespace bot.Controllers
                         parseMode: ParseMode.Html, replyMarkup: new InlineKeyboardMarkup(buttons));
                     break;
                 default:
-                    await
-                        _telegramClient.SendTextMessageAsync(message.Chat.Id,
-                        "Напишите сообщение", cancellationToken: ct);
+                    string userLanguageCode = _memoryStorage.GetSession(message.Chat.Id).ActionCode;
+                    if (userLanguageCode == "*")
+                    {
+                        await _telegramClient.SendTextMessageAsync(message.Chat.Id,
+                        $"Длина сообщения: {message.Text.Length}.", cancellationToken: ct);
+                    }
+
                     break;
             }
         }
