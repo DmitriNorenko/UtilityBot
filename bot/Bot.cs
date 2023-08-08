af5ffce3c6681c62486dc0e2a3d2c747e1f45268
Name <email@exaple.ru>
2023-08-08 07:54:59 +0300

    Добавьте файлы проекта.

---
+++ bot/Bot.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Extensions.Hosting;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace bot
+{
+    internal class Bot : BackgroundService
+    {
+        private ITelegramBotClient _telegramClient;
+
+        public Bot(ITelegramBotClient telegramClient)
+        {
+            _telegramClient = telegramClient;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _telegramClient.StartReceiving(
+                HandleUpdateAsync,
+                HandleErrorAsync,
+                new ReceiverOptions() { AllowedUpdates = { } }, // Здесь выбираем, какие обновления хотим получать. В данном случае разрешены все
+                cancellationToken: stoppingToken);
+
+            Console.WriteLine("Бот запущен");
+        }
+
+        async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            if (update.Type == UpdateType.Message)
+                if (update.Message.Type == MessageType.Text)
+                {
+                    await _telegramClient.SendTextMessageAsync(update.Message.Chat.Id,
+                        $"Длина сообщения: {update.Message.Text.Length} знаков", cancellationToken: cancellationToken);
+                }
+                else 
+                {
+                    await _telegramClient.SendTextMessageAsync(update.Message.Chat.Id,
+                        "Вы отправили не строку.", cancellationToken: cancellationToken);
+                }
+        }
+
+        Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            var errorMessage = exception switch
+            {
+                ApiRequestException apiRequestException
+                    => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+
+            Console.WriteLine(errorMessage);
+
+            Console.WriteLine("Ожидаем 10 секунд перед повторным подключением.");
+            Thread.Sleep(10000);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ bot/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using bot;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Telegram.Bot;
+
+namespace bot
+{
+    public class Program
+    {
+        public static async Task Main()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+
+            var host = new HostBuilder()
+                .ConfigureServices((hostContext, services) => ConfigureServices(services)) 
+                .UseConsoleLifetime() 
+                .Build(); 
+
+            Console.WriteLine("Сервис запущен");
+
+            await host.RunAsync();
+            Console.WriteLine("Сервис остановлен");
+        }
+
+        static void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddSingleton<ITelegramBotClient>(provider => new TelegramBotClient("6200167109:AAExqmCiop9WY8KLuSgO7WyZIZizG39yVzc"));
+
+            services.AddHostedService<Bot>();
+        }
+    }
+}
